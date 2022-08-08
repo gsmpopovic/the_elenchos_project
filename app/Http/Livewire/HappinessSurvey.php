95d9c55201c2de24c0622ac6424a174d151f764b
George Popovic <georgesmpopovic@gmail.com>
2022-08-08 11:10:18 -0400

    happiness survey lw comp

---
+++ app/Http/Livewire/HappinessSurvey.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HappinessSurvey extends Component
+{
+    public function render()
+    {
+        return view('livewire.happiness-survey');
+    }
+}
